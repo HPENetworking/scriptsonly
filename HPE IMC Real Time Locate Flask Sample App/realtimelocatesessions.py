b130bf8de38a3c00cf84fa7fa4ab29bb2bb71295
netmanchris <chris_young@me.com>
2016-01-21 16:40:23 -0500

    Added Apache2 License and removed partner provided scripts

--- HPE IMC Real Time Locate Flask Sample App/realtimelocatesessions.py
+++ HPE IMC Real Time Locate Flask Sample App/realtimelocatesessions.py
@@ -1,3 +1,4 @@
+'''
  Copyright 2016 Hewlett Packard Enterprise Development LP.
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +13,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 
-'''
+
 __author__ = "@netmanchris"
 __copyright__ = "Copyright 2016, Hewlett Packard Enterprise Development LP."
 __credits__ = ["Chris Young"]
--- get_netconf_VLANs_HPN.py
+++ get_netconf_VLANs_HPN.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
+''' Copyright 2016 Hewlett Packard Enterprise Development LP.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.'''
+
+
 '''
 This is simple script to retrieve information from HPN switch or router via NETCONF over SSH.
 This program can be easily extended to meet your own requirements. I have developed the program with
@@ -9,7 +24,7 @@ please change h3c.com in hp.com.
 
 __author__ = "Dobias van Ingen"
 __credits__ = "Jason Edelman for insipring me in creating this program (http://www.jedelman.com/)"
-__license__ = "MIT"
+__license__ = "Apache2"
 __version__ = "0.3"
 __maintainer__ = "Dobias van Ingen"
 __email__ = "dobias.vaningen@gmail.com"
