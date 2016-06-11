8e596b026da227d144ab037d1429bd53ce3de665
netmanchris <chris_young@me.com>
2016-06-11 16:09:43 -0400

    Added SpineLeaf generation via Jinja2 template

---
+++ Events/ETSS June 2016/DevOps Networking Model/Generate_Spine_Leaf_Configs/AutoL3Fabric.py
@@ -0,0 +1,31 @@
+from jinja2 import Environment, FileSystemLoader, Template
+import yaml
+import json
+
+ENV = Environment(loader=FileSystemLoader('./'))
+
+with open("./Inputs/globalvals.yaml") as inputfile:
+    devglobals =  yaml.load(inputfile)
+
+with open('./Inputs/devices2.yaml') as inputfile:
+    devs = yaml.load(inputfile)
+
+for dev in devs:
+    if dev['role'] == "spine":
+        template = ENV.get_template("./Templates/7900_spine.j2")
+        #print (template.render(devglobals=devglobals, dev=dev))
+        with open(dev['sysname']+".cfg", "w") as file:
+            file.write(template.render(devglobals=devglobals, dev=dev))
+    if dev['role'] == "leaf":
+        template = ENV.get_template("./Templates/5930_leaf.j2")
+    #print (template.render(devglobals=devglobals, dev=dev))
+    with open("./Configs/"+dev['sysname']+".cfg", "w") as file:
+        file.write(template.render(devglobals=devglobals, dev=dev))
+
+# Print dictionary generated from yaml    
+
+
+# Render template and print generated config to console
+#template = ENV.get_template("Comware5_Template.j2")
+#print (template.render(network_global=network_global, device_values = device_values, site= site))
+
