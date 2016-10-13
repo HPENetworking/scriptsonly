558ace22a3dcf6fba69bc34263e434af5c48cb81
joe-neville <joe.neville1@gmail.com>
2016-10-13 10:32:42 +0100

    Add netmiko sample dir

---
+++ netmiko_scripts/dis_arp.py
@@ -0,0 +1,45 @@
+from netmiko import ConnectHandler
+from datetime import datetime
+
+MSR1K_1 = {
+    'device_type':'hp_comware',
+    'ip':'10.150.0.2',
+    'username':'ctrask',
+    'password':'timshel',
+}
+
+A3810_1 = {
+    'device_type':'hp_procurve',
+    'ip':'10.150.0.1',
+    'username':'ctrask',
+    'password':'timshel',
+}
+
+#vsrs = [VSR301, VSR104]
+tin = [A3810_1, MSR1K_1]
+
+
+start_time = datetime.now()
+print start_time
+fail_list = []
+for x in tin:
+    net_connect = ConnectHandler(**x)
+    output = net_connect.send_command("display arp")
+    name = net_connect.find_prompt()
+    print "\n\n>>>>>>>>>>>>>>>{0}<<<<<<<<<<<<<<<<<<<<".format(x['ip'])
+    print output
+    if '10.150.0.254' in output:
+        print "GATEWAY OK"
+    else:
+        print "GATEWAY UNKNOWN"
+        fail_list.append(x['ip'])
+
+    print (">>>>>>>>>>>>>>>>>>>>>End<<<<<<<<<<<<<<<<<<<<<<")
+
+end_time = datetime.now()
+print end_time
+if fail_list != []:
+    print "\nThese devices failed:"
+    print fail_list
+else:
+    print "\n\nAll devices OK"
