83d962e63eedc72e0665cecc0f9ae2c7317d8ec1
joe-neville <joe.neville1@gmail.com>
2017-09-11 16:12:52 +0100

    Create multi device examples of anycli using YAML and for loop  Changes to be committed: 	modified:   multi_anycli_yaml.py 	renamed:    multi_yaml_test.yaml -> multi_anycli_yaml.yaml

--- ArubaOS-Sw_API_Scripts/pyarubaos-sw_api/yaml_example/multi_anycli_yaml.py
+++ ArubaOS-Sw_API_Scripts/pyarubaos-sw_api/yaml_example/multi_anycli_yaml.py
@@ -1,14 +1,44 @@
+import requests
+import json
+import base64
 import yaml
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-with open("yaml_test.yaml", 'r') as stream:
+with open("multi_anycli_yaml.yaml", 'r') as stream:
     try:
-        pydata = yaml.load(stream)
-        print(pydata)
-        ip_addr = pydata['ip_addr']
-        username = pydata['username']
-        password = pydata['password']
-        command = pydata['command']
+        yaml_data = yaml.load(stream)
+        ip_addr = yaml_data['ip_addr']
+        username = yaml_data['username']
+        password = yaml_data['password']
+        command = yaml_data['command']
     except yaml.YAMLERROR as exc:
         print(exc)
 
-print(ip_addr)
+
+
+
+for switch in ip_addr:
+    url = 'http://' + switch + '/rest/v3/'
+    creds = {'userName': username, 'password': password}
+    s = requests.Session()
+    r = s.post(url + 'login-sessions', data=json.dumps(creds), timeout=1)
+    cookie_response = r.json()['cookie']
+    if r.status_code != 201:
+        print('Login error, status code {}'.format(r.status_code))
+
+
+    cookie = {'cookie': cookie_response}
+    c = {'cmd': command}
+    post_command = requests.post(url + 'cli', headers=cookie, data=json.dumps(c), timeout=1)
+
+
+    if post_command.status_code != 200:
+        print(('Error, status code {}'.format(post_command.status_code)))
+    else:
+        print('Status Code: ' + str(post_command.status_code))
+        response = post_command.json()['result_base64_encoded']
+        decoded_r = base64.b64decode(response).decode('utf-8')
+        print(decoded_r)
+        print('*' * 80)
+
